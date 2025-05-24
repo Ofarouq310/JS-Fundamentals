6e3400100ca39abdd3f517de34af02da43f1f36c
Omar Farouq <ofarouq310@gmail.com>
2025-05-24 19:16:00 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,20 +1,9 @@
-const readline = require('readline');
+const args = process.argv.slice(2); // Skip 'node' and script path
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Enter arguments separated by spaces: ", function(input) {
-  const args = input.trim().split(/\s+/);
-
-  if (args.length === 0 || args[0] === "") {
-    console.log("No argument");
-  } else if (args.length === 1) {
-    console.log("Argument found");
-  } else {
-    console.log("Arguments found");
-  }
-
-  rl.close();
-});
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
