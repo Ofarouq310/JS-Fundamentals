b13cee858833c0c200e8b1914eebaad5db3e96b4
Omar Farouq <ofarouq310@gmail.com>
2025-05-24 19:29:30 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,17 +1,12 @@
 const arg = process.argv[2];
 const x = parseInt(arg, 10);
 
-if (isNaN(x) || x < 1) {
-  console.log("");
-  process.exit(0);
+if (!isNaN(x) && x > 0) {
+  let i = 0;
+  let output = "";
+  while (i < x) {
+    output += "C is fun\n";
+    i++;
+  }
+  console.log(output.trim());
 }
-
-let i = 0;
-let output = "";
-
-while (i < x) {
-  output += "C is fun\n";
-  i++;
-}
-
-console.log(output.trim());
